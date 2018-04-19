16eb3a203a37ccc30d5062c744a27042e352ee42
unknown <admin@VLAN-25-58-049.rd.mirea.ru>
2018-04-19 14:14:21 +0300

    Initial commit

---
+++ src/main/java/ru/mirea/MyTodoModel.java
@@ -0,0 +1,39 @@
+package ru.mirea;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by admin on 19.04.2018.
+ */
+public class MyTodoModel implements TodoModel {
+    private List<TodoItem> items = new ArrayList<>();
+    private int count = 1;
+
+    public MyTodoModel() {
+        add("7777");
+        add("9999");
+    }
+
+    @Override
+    public List<TodoItem> getItems() {
+        return items;
+    }
+
+    @Override
+    public void add(String text) {
+        items.add(new TodoItem(count, text));
+        count++;
+    }
+
+    @Override
+    public void delete(int id) {
+        for (int i = 0; i < items.size(); i++){
+            TodoItem item = items.get(i);
+            if (item.getId() == id){
+                items.remove(i);
+                break;
+            }
+        }
+    }
+}
